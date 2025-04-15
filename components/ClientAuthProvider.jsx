7e1a3980680a98b4bd07afeedb5dd642c5f86bc6
Mohsin Furkh <mohsinfaurkh@gmail.com>
2025-04-15 18:13:18 +0530

    Fix deployment issues: Updated Next.js config, fixed imports, added AuthContext to app directory

--- components/ClientAuthProvider.jsx
+++ components/ClientAuthProvider.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AuthProvider } from '../contexts/AuthContext';
+import { AuthProvider } from '../app/contexts/AuthContext';
 
 export default function ClientAuthProvider({ children }) {
   return <AuthProvider>{children}</AuthProvider>;
