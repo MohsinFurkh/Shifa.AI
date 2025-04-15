94b24f28b235ef20ff0575c62beaf5eb7a08d86b
Mohsin Furkh <55917187+MohsinFurkh@users.noreply.github.com>
2025-04-16 04:25:39 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // Remove the deprecated options
-    serverComponentsExternalPackages: ['mongoose'],
-  },
+  // Use the new serverExternalPackages option instead of the deprecated one
+  serverExternalPackages: ['mongoose'],
   webpack: (config) => {
     // This is to handle the native dependencies
     config.externals.push({
@@ -13,5 +11,4 @@ const nextConfig = {
     return config;
   },
 };
-
-module.exports = nextConfig; 
+module.exports = nextConfig;
