0944ebc20b75c5623c19739ac29f4a417133856c
Mohsin Furkh <55917187+MohsinFurkh@users.noreply.github.com>
2025-04-16 04:14:50 +0530

    Update LoginForm.jsx

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -155,7 +155,7 @@ export default function LoginForm() {
                   onChange={(e) => setUserType(e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6 px-2"
                 >
-                  <option value="patient">Patient</option>
+                  <option value="patient">User</option>
                   <option value="doctor">Doctor</option>
                   <option value="admin">Admin</option>
                 </select>
